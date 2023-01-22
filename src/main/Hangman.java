0cabd42cd05831495446110c369e434e9d14164d
Juan Luis <juanluiscr27@hotmail.com>
2023-01-21 23:55:29 -0500

    Write the test case for a function that count how many times a character is in a word

---
+++ src/main/Hangman.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Hangman {
+    public int countAlphabet(String word, char alphabet) {
+        // TODO
+        return 0;
+    }
+}
--- src/test/HangmanTest.java
+++ src/test/HangmanTest.java
@@ -1,7 +1,19 @@
 package test;
 
+import main.Hangman;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HangmanTest {
+    @Test
+    void test_alphabetCountInAward(){
+        String word = "pizza";
+        char alphabet = 'a';
 
+        Hangman hangman = new Hangman();
+
+        int count = hangman.countAlphabet(word, alphabet);
+        assertEquals(1, count);
+    }
 }
